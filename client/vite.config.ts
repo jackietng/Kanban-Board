e9d4736c5ae488e6b3da57f3bdb3e0a0a0fa2956
jackietng <jn1thythy56@gmail.com>
2025-01-16 20:56:11 -0700

    troubleshooting deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-  },
   server: {
     port: 3000,
     open: true,
